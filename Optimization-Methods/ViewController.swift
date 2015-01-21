295b54f6eb7fd8893b853758627e7a26ff79a41c
Hani Kazmi <hanikazmi@me.com>
2015-01-21 15:01:56 +0000

    Initial Commit

---
+++ Optimization-Methods/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Optimization-Methods
+//
+//  Created by Hani Kazmi on 21/01/2015.
+//  Copyright (c) 2015 Hani Kazmi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
